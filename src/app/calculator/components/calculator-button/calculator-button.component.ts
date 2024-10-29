91e5b17911fe6c467f79704d62d71a02a5c7faaa
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-10-29 13:35:06 +0100

    button component fix class

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  input,
+} from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -6,10 +11,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './calculator-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './calculator-button.component.scss',
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400',
     //attribute: 'hola',  se puede poner mas variables al contenedor del componente
     //'data-size': 'XL',
   }
 })
-export class CalculatorButtonComponent { }
+export class CalculatorButtonComponent {
+  public isCommand = input( false , {
+      transform: ( value: boolean | string ) =>
+        typeof value === 'string' ? value === '' : value,
+    })
+
+    @HostBinding('class.is-command') get commandStyle() {
+      return this.isCommand();
+    }
+}
