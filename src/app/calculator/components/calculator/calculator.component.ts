37126539ca4faf520dd5cde032a20afd888dd0f6
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-10-29 13:13:03 +0100

    wip

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 @Component({
   selector: 'calculator',
   standalone: true,
-  imports: [],
+  imports: [
+    CalculatorButtonComponent
+  ],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
