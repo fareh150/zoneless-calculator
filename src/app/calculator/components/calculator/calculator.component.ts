6d21d4c9ece58f8ddc91d1060cafc1868ededc28
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-10-29 17:42:06 +0100

    get buton value  from keyboard and usage

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -9,10 +9,25 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   ],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
   // styles: `
   //   .is-command {
   //     @apply bg-indigo-700 bg-opacity-20;
   //   }
   // `,
 })
-export class CalculatorComponent { }
+export class CalculatorComponent
+{
+  handleClick(key: string): void
+  {
+    console.log({key});
+  }
+
+  //@HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent( event: KeyboardEvent )
+  {
+    this.handleClick( event.key );
+  }
+}
