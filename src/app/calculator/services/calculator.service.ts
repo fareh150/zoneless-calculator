3689e8981ff54faa509d797b445bfde83b34c18a
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-10-30 15:24:29 +0100

    service controll settings

--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -1,5 +1,12 @@
 import { Injectable, signal } from '@angular/core';
 
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const operators = ['+', '-', 'x', '÷', '*', '/']; // aqui poner * o /
+
+const specialOperators = ['C', '+/-', '=', '%', '.', 'Backspace'];
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,4 +15,66 @@ export class CalculatorService
   public resultText = signal('0');
   public subResultText = signal('0');
   public lastOperator = signal('+');
+
+  public constructNumber ( value:string ): void
+  {
+    // Validar input
+    if ( ![...numbers, ...operators, ...specialOperators].includes(value) )
+    {
+      console.log('Invalid input');
+      return;
+    }
+
+    if ( value === '=' )
+    {
+      console.log('Calculate result');
+      return;
+    }
+
+    // Clear  all
+    if ( value === 'C' )
+    {
+      this.resultText.set('0');
+      this.subResultText.set('0');
+      this.lastOperator.set('+');
+      return;
+    }
+
+    // Backspace
+    // Todo : revisar negativos
+    if ( value === 'Backspace' )
+    {
+      if ( this.resultText() === '0' ) return;
+      if ( this.resultText().length === 1 )
+      {
+        this.resultText.set('0');
+        return;
+      }
+
+      this.resultText.update( (prev) => prev.slice(0, -1) );
+      console.log('Backspace');
+      return;
+    }
+
+    // Aplicar operadores
+    if ( operators.includes(value) )
+    {
+      this.lastOperator.set(value);
+      this.subResultText.set(this.resultText());
+      this.resultText.set('0');
+      return;
+    }
+
+    // Validar decimal
+    if ( value === '.' && !this.resultText().includes('.') )
+    {
+      if ( this.resultText() === '0' || this.resultText() === '' )
+      {
+        this.resultText.update( (text) => text + '0.' );
+        return;
+      }
+      this.resultText.update( (text) => text + '.' );
+      return;
+    }
+  }
 }
