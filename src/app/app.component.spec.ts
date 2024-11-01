c3efd027a3a4ae4f78574ee540fac23bc591b67f
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-01 20:25:13 +0100

    ficxture and compiled unification
    -los ponemos en el beforeEach así los tenemos activos para todos los test que necesitemos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -28,15 +34,11 @@ describe('AppComponent', () => {
   })
 
    it(`should have the 'zoneless-calculator' title`, () => {
-     const fixture = TestBed.createComponent(AppComponent);
      const app = fixture.componentInstance;
      expect(app.title).toEqual('zoneless-calculator');
    });
 
-  //  it('should render title', () => {
-  //    const fixture = TestBed.createComponent(AppComponent);
-  //    fixture.detectChanges();
-  //    const compiled = fixture.nativeElement as HTMLElement;
-  //    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
-  //  });
+    it('should render title', () => {
+      //expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+    });
 });
