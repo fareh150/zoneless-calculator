9902cebe9ad700995a2e214c1f82bc66800f3e87
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-01 18:47:48 +0100

    wip

--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -16,23 +16,23 @@ export class CalculatorService
   public subResultText = signal('0');
   public lastOperator = signal('+');
 
-  public constructNumber ( value:string ): void
+  public constructNumber (value:string): void
   {
     // Validar input
-    if ( ![...numbers, ...operators, ...specialOperators].includes(value) )
+    if (![...numbers, ...operators, ...specialOperators].includes(value))
     {
       console.log('Invalid input');
       return;
     }
 
-    if ( value === '=' )
+    if (value === '=')
     {
       console.log('Calculate result');
       return;
     }
 
     // Clear  all
-    if ( value === 'C' )
+    if (value === 'C')
     {
       this.resultText.set('0');
       this.subResultText.set('0');
@@ -42,22 +42,22 @@ export class CalculatorService
 
     // Backspace
     // Todo : revisar negativos
-    if ( value === 'Backspace' )
+    if (value === 'Backspace')
     {
-      if ( this.resultText() === '0' ) return;
-      if ( this.resultText().length === 1 )
+      if (this.resultText() === '0') return;
+      if (this.resultText().length === 1)
       {
         this.resultText.set('0');
         return;
       }
 
-      this.resultText.update( (prev) => prev.slice(0, -1) );
+      this.resultText.update((prev) => prev.slice(0, -1));
       console.log('Backspace');
       return;
     }
 
     // Aplicar operadores
-    if ( operators.includes(value) )
+    if (operators.includes(value))
     {
       this.lastOperator.set(value);
       this.subResultText.set(this.resultText());
@@ -65,49 +65,49 @@ export class CalculatorService
       return;
     }
     // limitar caracteres
-    if ( this. resultText().length >= 8 )
+    if (this. resultText().length >= 8)
     {
       console.log('Max length reached');
       return;
     }
 
     // Decimal
-    if ( value === '.' || value === ',' && !this.resultText().includes('.') )
+    if (value === '.' || value === ',' && !this.resultText().includes('.'))
     {
-      if ( this.resultText() === '0' || this.resultText() === '' )
+      if (this.resultText() === '0' || this.resultText() === '')
         {
           this.resultText.set('0.');
           return;
         }
       return;
     }
-    if ( value === '.' || value === ','  && this.resultText().includes('.') )
+    if (value === '.' || value === ','  && this.resultText().includes('.'))
     {
       return;
     }
 
     // manage initial 0
-    if ( value === '0' && (this.resultText() === '0'|| this.resultText() === '-0') )
+    if (value === '0' && (this.resultText() === '0'|| this.resultText() === '-0'))
     {
       return;
     }
 
     // cambiar signo
-    if ( value === '+/-' )
+    if (value === '+/-')
     {
-      if ( this.resultText().includes('-') )
+      if (this.resultText().includes('-'))
       {
-        this.resultText.update( (text) => text.slice(1) );
+        this.resultText.update((text) => text.slice(1));
         return;
       }
-      this.resultText.update( text => '-' + text );
+      this.resultText.update(text => '-' + text);
       return;
     }
 
     // numbers
-    if ( numbers.includes(value) )
+    if (numbers.includes(value))
     {
-      if( this.resultText() === '0' || this.resultText() === '-0' )
+      if(this.resultText() === '0' || this.resultText() === '-0')
       {
         if (this.resultText() === '-0')
         {
