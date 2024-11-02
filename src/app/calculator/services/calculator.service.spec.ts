bbd744bdb0a6aae8318e5a4b211ded3d5b2535be
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-02 11:17:03 +0100

    more tests

--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -35,4 +35,88 @@ describe('CalculatorService', () =>
     expect(service.subResultText()).toBe('0');
     expect(service.lastOperator()).toBe('+');
   });
+
+  it('should update resultText with number imput', () =>
+  {
+    service.constructNumber('1');
+    expect(service.resultText()).toBe('1');
+
+    service.constructNumber('2');
+    expect(service.resultText()).toBe('12');
+
+    service.constructNumber('3');
+    expect(service.resultText()).toBe('123');
+  });
+
+  it('should handle operators correctly', () =>
+  {
+    service.constructNumber('1');
+    service.constructNumber('-');
+
+    expect(service.lastOperator()).toBe('-');
+    expect(service.subResultText()).toBe('1');
+    expect(service.resultText()).toBe('0');
+  });
+
+  it('shold calculate result correctly for addition', () =>
+  {
+    service.constructNumber('1');
+    service.constructNumber('+');
+    service.constructNumber('2');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('3');
+  });
+
+  it('shold calculate result correctly for substraction', () =>
+  {
+    service.constructNumber('3');
+    service.constructNumber('-');
+    service.constructNumber('2');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('1');
+  });
+
+  it('shold calculate result correctly for multiplication', () =>
+  {
+    service.constructNumber('3');
+    service.constructNumber('x');
+    service.constructNumber('2');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('6');
+  });
+
+  it('shold calculate result correctly for division', () =>
+  {
+    service.constructNumber('6');
+    service.constructNumber('÷');
+    service.constructNumber('2');
+    service.constructNumber('=');
+
+    expect(service.resultText()).toBe('3');
+  });
+
+  it('shoould handle decimal point correctly', () =>
+  {
+    service.constructNumber('1');
+    service.constructNumber('.');
+    service.constructNumber('2');
+
+    expect(service.resultText()).toBe('1.2');
+    service.constructNumber('.');
+    expect(service.resultText()).toBe('1.2');
+  });
+
+  it('shoould handle decimal point correctly starting with 0', () =>
+    {
+      service.constructNumber('0');
+      service.constructNumber('.');
+      service.constructNumber('0');
+
+      expect(service.resultText()).toBe('0.0');
+      service.constructNumber('.');
+      expect(service.resultText()).toBe('0.0');
+    });
 });
