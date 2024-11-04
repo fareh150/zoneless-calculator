25eb6213da2e7fc1ff9c94e03f058655662ce7be
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-04 16:02:39 +0100

    create a mock and use it

--- src/app/calculator/components/calculator/calculator.component.spec.ts
+++ src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -1,5 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorComponent } from './calculator.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
+
+class MockCalculatorService
+{
+  public resultText = jasmine.createSpy('resultText').and.returnValue('100.00');
+  public subResultText = jasmine.createSpy('subResultText').and.returnValue('0');
+  public lastOperator = jasmine.createSpy('lastOperator').and.returnValue('+');
+
+  public constructNumber = jasmine.createSpy('constructNumber');
+}
 
 describe('CalculatorComponent', () => {
 
@@ -10,6 +20,12 @@ describe('CalculatorComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CalculatorComponent],
+      providers: [
+        {
+          provide: CalculatorService,
+          useClass: MockCalculatorService,
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CalculatorComponent);
