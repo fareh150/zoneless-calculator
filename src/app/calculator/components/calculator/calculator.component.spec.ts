c68f24d6fff84565dab91dd1aab9e047f5fbf519
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-05 13:32:19 +0100

    mock usage editing and check

--- src/app/calculator/components/calculator/calculator.component.spec.ts
+++ src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -5,7 +5,7 @@ import { CalculatorService } from '@/calculator/services/calculator.service';
 class MockCalculatorService
 {
   public resultText = jasmine.createSpy('resultText').and.returnValue('100.00');
-  public subResultText = jasmine.createSpy('subResultText').and.returnValue('0');
+  public subResultText = jasmine.createSpy('subResultText').and.returnValue('20');
   public lastOperator = jasmine.createSpy('lastOperator').and.returnValue('+');
 
   public constructNumber = jasmine.createSpy('constructNumber');
@@ -16,6 +16,8 @@ describe('CalculatorComponent', () => {
   let fixture: ComponentFixture<CalculatorComponent>;
   let compiled: HTMLElement;
   let component: CalculatorComponent;
+
+  let mockCalculatorService: MockCalculatorService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -32,7 +34,12 @@ describe('CalculatorComponent', () => {
     compiled = fixture.nativeElement as HTMLElement;
     component = fixture.componentInstance;
 
-    fixture.detectChanges();
+    // Get the provided MockCalculatorService
+    mockCalculatorService = TestBed.inject(
+      CalculatorService
+    ) as unknown as MockCalculatorService;
+
+    //fixture.detectChanges();
   });
 
   it('should create the app', () => {
@@ -44,7 +51,23 @@ describe('CalculatorComponent', () => {
   it('should have the current getters', () =>
   {
     expect(component.resultText()).toBe('100.00');
-    expect(component.subResultText()).toBe('0');
+    expect(component.subResultText()).toBe('20');
     expect(component.lastOperator()).toBe('+');
   });
+
+  it('should display calculation values', () =>
+    {
+      mockCalculatorService.resultText.and.returnValue('123');
+      mockCalculatorService.subResultText.and.returnValue('456');
+      mockCalculatorService.lastOperator.and.returnValue('*');
+
+      fixture.detectChanges();
+
+      expect(compiled.querySelector('span')?.innerText).toBe('456 *');
+
+
+      expect(component.resultText()).toBe('123');
+      expect(component.subResultText()).toBe('456');
+      expect(component.lastOperator()).toBe('*');
+    });
 });
